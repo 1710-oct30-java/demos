eb88b7dad108b53e53fa63ab858a48056dc24b5e
DESKTOP-AD7EOVG\Saad <saad2139@gmail.com>
2017-10-31 13:29:56 -0400

    Created packages for each completed problem

--- assignments/homeworkOne/src/homeworkOne/NumberOne.java
+++ assignments/homeworkOne/src/homeworkOne/NumberOne.java
@@ -1,28 +1,5 @@
 package homeworkOne;
 
 public class NumberOne {
-	//Perform a bubble sort on the following integer array: 1,0,5,6,3,2,3,7,9,8,4
-	public static void main(String[] args)
-	{
-	int bubbleSort[] = {1,0,5,6,3,2,3,7,9,8,4};
-	int temp;
-	for(int i = 0; i<bubbleSort.length; i++)
-	{
-		for(int j = 0; j<bubbleSort.length-1; j++)
-		{
-			if(bubbleSort[i] > bubbleSort[j])
-			{
-				temp = bubbleSort[i];
-				bubbleSort[i] = bubbleSort[j];
-				bubbleSort[j] = temp;
-			}
-		}
-		
-	}
-	for(int i=0 ; i<bubbleSort.length;i++)
-	{
-		System.out.println(bubbleSort[i]);
-	}
-	
-	}
+
 }
--- assignments/homeworkOne/src/homeworkOne/NumberTwo.java
+++ assignments/homeworkOne/src/homeworkOne/NumberTwo.java
@@ -4,25 +4,7 @@ public class NumberTwo {
 	
 	
 	
-	public static void main(String[] args)
-	{
-		int fibNumber = 26;
-		
-		for(int i = 0; i<fibNumber;i++)
-		{
-			System.out.print(fib(i) +" ");
-		}
-		
-	}
 	
-	public static int fib(int fibNumber)
-	{
-		if( fibNumber == 0 || fibNumber == 1)
-			return fibNumber;
-		else
-			return fib(fibNumber-1) + fib(fibNumber-2);
-			
-	}
 	
 
 }
