2029a160fbb59f4e4f0e451d3161a551780e6c89
Dennis Heinle <dennarai@outlook.com>
2017-11-01 16:21:19 -0400

    Update Q5

--- assignments/src/assignment1/Question5.java
+++ assignments/src/assignment1/Question5.java
@@ -8,15 +8,17 @@ public class Question5
 	// StringBuilder or StringBuffer APIs.
 	public static void main(String[] args)
 	{
-		
+		System.out.println(subString("Hai frens!", 9));
 	}
 	
-	public String subString(String str, int idx)
+	public static String subString(String str, int idx)
 	{
-		String output = null;
+		String output = "";
 		
-		
-		
+		for(int x = 0; x <= idx; x++)
+		{
+			output = output + str.charAt(x);
+		}
 		return output;
 	}
 }
