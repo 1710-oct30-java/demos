82f29f02590e85852c7c8e153e7ceaddf6b6806e
Dennis Heinle <dennarai@outlook.com>
2017-11-01 16:49:25 -0400

    Q7 work

--- assignments/src/assignment1/Question7.java
+++ assignments/src/assignment1/Question7.java
@@ -1,10 +1,60 @@
 package assignment1;
 
-public class Question7
+import java.util.Comparator;
+
+public class Question7 implements Comparator
 {
 	// Sort two employees based on their name, department and age using the Comparator interface.
 	public static void main(String[] args)
 	{
 		
+	}
+	
+	class Employee
+	{
+		String name = "";
+		String department = "";
+		int age = 0;
+		
+		public Employee()
+		{
+		}
+		
+		public Employee(String n, String d, int a)
+		{
+			this.name = n;
+			this.department = d;
+			this.age = a;
+		}
+		
+		public String getName()
+		{
+			return this.name;
+		}
+		
+		public String getDepartment()
+		{
+			return this.department;
+		}
+		
+		public int getAge()
+		{
+			return this.age;
+		}
+		
+		public void setName(String n)
+		{
+			this.name = n;
+		}
+		
+		public void setDepartment(String d)
+		{
+			this.department = d;
+		}
+		
+		public void setAge(int a)
+		{
+			this.age = a;
+		}
 	}
 }
