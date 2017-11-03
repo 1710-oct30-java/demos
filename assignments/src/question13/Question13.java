0c634f1e32e50ee57bdbc21eecb17bc5f48a4b2d
Dennis Heinle <dennarai@outlook.com>
2017-11-02 22:48:09 -0400

    Updated Q14

--- assignments/src/question13/Question13.java
+++ assignments/src/question13/Question13.java
@@ -5,8 +5,20 @@ public class Question13
 	// Display the triangle on the console as follows using
 	// any type of loop. Do NOT use a simple group of print
 	// statements to accomplish this.
+	//
+	// 0
+	// 1 0
+	// 1 0 1
+	// 0 1 0 1
 	public static void main(String[] args)
 	{
+		// Initialize starting state of output.
+		String output = "";
 		
+		for(int x = 0; x < 4; x++)
+		{
+			output = output + "0";
+			
+		}
 	}
 }
---
+++ assignments/src/question14/Question14.java
@@ -0,0 +1,34 @@
+package question14;
+
+import java.time.LocalDateTime;
+
+public class Question14
+{
+	// Write a program that demonstrates the switch case.
+	// Implement the following functionalities in the cases:
+	// Case 1: Find the square root of a number using the Math
+	// class method.
+	// Case 2: Display today's date.
+	// Case 3: Split the following string and store it in a
+	// string array.
+	// "I am learning Core Java"
+	public static void main(String[] args)
+	{
+		int choice = 3;
+		switch(choice)
+		{
+		case 1:
+			int num = 9;
+			System.out.println("The square root of " + num + " is " + Math.sqrt(num) + ".");
+			break;
+		case 2:
+			System.out.println("Today's date is " + LocalDateTime.now());
+			break;
+		case 3:
+			String str = "I am learning Core Java";
+			String[] ary = str.split("\\s+");
+			System.out.println(ary.toString());
+			break; 
+		}
+	}
+}
