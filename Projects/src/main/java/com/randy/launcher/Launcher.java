7579b6277ced9a3178110ba7b987d5ec5129a060
randythiencao <randythiencao@yahoo.com>
2017-11-15 09:00:00 -0500

    synced

---
+++ Projects/src/main/java/com/randy/launcher/Launcher.java
@@ -0,0 +1,12 @@
+package com.randy.launcher;
+
+import org.apache.log4j.Logger;
+
+public class Launcher
+{
+	private static Logger log = Logger.getRootLogger();
+	public static void main(String[] args)
+	{
+		
+	}
+}
---
+++ Projects/src/main/java/com/randy/servlets/LoginServlet.java
@@ -0,0 +1,7 @@
+package com.randy.servlets;
+import org.apache.catalina.servlets.DefaultServlet;
+
+public class LoginServlet extends DefaultServlet
+{
+	
+}
