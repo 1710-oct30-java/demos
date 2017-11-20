d8f675e51ea85fa8a302a5445f5cc9378297b482
Pascal Parsard <pparsard@gmail.com>
2017-11-19 22:07:17 -0500

    more javascript, sql packet in last post was a typo

--- html/packet.js
+++ html/packet.js
@@ -210,7 +210,10 @@ Define a function Person(name, age)
 The following line should set a Person object to the variable john:
 	var john = new Person("John", 30);
 */
-
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
 /*
 13. Defining an object using an object literal
 Define function getPerson(name, age)
