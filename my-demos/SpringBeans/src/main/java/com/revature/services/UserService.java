8b97975bb730017682221af9d26e6db9a10e9599
Blake Kruppa <btkruppa513@gmail.com>
2017-12-06 11:10:44 -0500

    added spring mvc demos

--- my-demos/SpringBeans/src/main/java/com/revature/services/UserService.java
+++ my-demos/SpringBeans/src/main/java/com/revature/services/UserService.java
@@ -1,14 +1,15 @@
 package com.revature.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.revature.repos.UserDao;
 
-@Component("user service")
+@Service("user service")
 public class UserService {
 
 	@Autowired
+	// can use @Qualifier if you need to specify a specific bean to be injected here
 	private UserDao ud;
 
 	public boolean login(String username, String password) {
